37cb02f7f87271b9e3dcbe282e707c0b03eb526c
al-masood <1903088@student.ruet.ac.bd>
2024-07-11 09:59:15 +0600

    Loading added

--- frontend/src/pages/CFRating.js
+++ frontend/src/pages/CFRating.js
@@ -1,14 +1,15 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Ranktable from "../components/Ranktable"
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
 
 const CodeforcesRating = () => {
-
     const [type, setType] = useState('')
     const [ranklist, setRanklist] = useState('')
-    
+    const [loading, setLoading] = useState(false)  // Add loading state
+
     const handleClick = async (clickType) => {
         setType(clickType)
+        setLoading(true)  // Set loading to true when fetch starts
         const response = await fetch(`${BACKEND_URL}/cfrating`, {
             method: 'POST',
             body: JSON.stringify({type}),
@@ -16,18 +17,25 @@ const CodeforcesRating = () => {
                 'Content-Type': 'application/json'
             }
         })
-         
+        
         const newRanklist = await response.json()
         setRanklist(newRanklist)
+        setLoading(false)  // Set loading to false when fetch completes
     }
 
-    return(
+    return (
         <div>
             <div className='option-button-group-large'>
                 <button className="option-button button" onClick={ () => handleClick('CurrentRating')}> Current Rating</button>
                 <button className="option-button button" onClick={ () => handleClick('MaxRating')}> Maximum Rating</button>
             </div>
-            <Ranktable finalRanklist={ranklist}/>
+            {loading ? (  // Conditionally render loading animation
+                <div className="loading-spinner-container">
+                    <div className="loading-spinner"></div>
+                </div>
+            ) : (
+                <Ranktable finalRanklist={ranklist}/>
+            )}
         </div>
     )
 }

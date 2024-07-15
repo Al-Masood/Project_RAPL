f872b03a938ac694a87108b185cc390ed1a4cfa3
al-masood <1903088@student.ruet.ac.bd>
2024-07-15 12:14:52 +0600

    navbar update

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -35,9 +35,6 @@ const Navbar = () => {
                 <Link to="/">
                     <img src={logo} alt="RAPL Logo" />
                 </Link>
-                <button className="mobile-menu-toggle" onClick={toggleMobileMenu}>
-                    &#9776;
-                </button>
             </div>
 
             <nav className={`navbar-links ${isMobileMenuOpen ? 'open' : ''}`}>
@@ -60,29 +57,32 @@ const Navbar = () => {
                         Admin Panel
                     </Link>
                 )}
+                <div className="navbar-user">
+                    {user ? (
+                        <>
+                            <Link to='/profile' onClick={() => setIsMobileMenuOpen(false)}>
+                                {user.name}
+                            </Link>
+                            <Link to="/" onClick={handleClick}>
+                                Logout
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/login" onClick={() => setIsMobileMenuOpen(false)}>
+                                Login
+                            </Link>
+                            <Link to="/signup" onClick={() => setIsMobileMenuOpen(false)}>
+                                Sign Up
+                            </Link>
+                        </>
+                    )}
+                </div>
             </nav>
 
-            <div className="navbar-user">
-                {user ? (
-                    <>
-                        <Link to='/profile'>
-                            {user.name}
-                        </Link>
-                        <Link to="/" onClick={handleClick}>
-                            Logout
-                        </Link>
-                    </>
-                ) : (
-                    <>
-                        <Link to="/login">
-                            Login
-                        </Link>
-                        <Link to="/signup">
-                            Sign Up
-                        </Link>
-                    </>
-                )}
-            </div>
+            <button className="mobile-menu-toggle" onClick={toggleMobileMenu}>
+                &#9776;
+            </button>
         </header>
     );
 };

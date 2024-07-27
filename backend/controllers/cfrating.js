21ddfae3aa01eb99dc5c02d824d41743476da949
al-masood <1903088@student.ruet.ac.bd>
2024-07-27 10:42:24 +0600

    Wrong Code

--- backend/controllers/cfrating.js
+++ backend/controllers/cfrating.js
@@ -1,37 +1,39 @@
-const users = require('../models/users');
-const axios = require('axios');
+const users = require('../models/users')
+const axios = require('axios')
 
-const updateRating = async () => {
-    try {
-        const fetchedUsers = await users.find({});
-        
-        if (fetchedUsers.length === 0) {
-            console.log('No users found');
-            return;
-        }
-
-        let apiURL = 'https://codeforces.com/api/user.info?handles=';
-
-        for (const user of fetchedUsers) {
-            const handle = user.cfHandle;
-            apiURL += handle + ';';
-        }
-
-        const response = await axios.get(apiURL);
-        const userData = response.data.result;
-
-        for (const user of userData) {
-            const { handle, rating, maxRating } = user;
-            await users.findOneAndUpdate(
-                { cfHandle: handle },
-                { rating, maxRating },
-                { new: true }
-            );
-        }
-
-    } catch (error) {
-        console.error('Error updating user ratings:', error);
+async function cfRating (req, res){
+    const type = req.body.type
+    const fetchedUsers = await users.find({})
+    
+    ranklist = []
+    for (const user of fetchedUsers){
+        ranklist.push([user.cfHandle, user.rating, user.maxRating])
     }
-};
 
-module.exports = updateRating;
+    if(type === 'CurrentRating'){
+        ranklist.sort((a, b) => {
+            if(a[1] == b[1]){
+                return b[2] - a[2]
+            }
+            return b[1] - a[1]
+        })
+    }
+
+    else{
+        ranklist.sort((a, b) => {
+            if(a[2] == b[2]){
+                return b[1] - a[1]
+            }
+            return b[2] - a[2]
+        })
+    }
+    
+    finalRanklist = []
+    finalRanklist.push(['Handle', 'Current Rating', 'Max Rating'])
+    for (const entries of ranklist) finalRanklist.push(entries)
+    
+    const jsonRanklist = JSON.stringify(finalRanklist)
+    res.send(jsonRanklist)
+}
+
+module.exports = cfRating
